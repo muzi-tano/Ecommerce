778f106c3dbf357bd5647adde76325a8269d0dd9
muzi-tano <muzitanoamatheus3@gmail.com>
2026-03-31 08:34:11 -0300

    Estilizando css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Header from './components/Header'
+import Capa from './components/Capa'
 
 function App() {
- 
-
   return (
     <div>
       <header>
         <Header/>
       </header>
+      <main>
+        <Capa/>
+      </main>
     </div>
   )
 }
--- src/components/Capa/index.jsx
+++ src/components/Capa/index.jsx
@@ -1,2 +1,18 @@
 import './Capa.css';
-import banner from '../../assets/banner.jpg';
+import banner from '../../assets/banner.png';
+import bannerMobile from '../../assets/banner_mobile.jpg';
+
+export default function Capa() {
+    return(
+        <section className='secao_capa'
+            style={{ backgroundImage: `url(${banner})`
+         }}>
+         <div className='texto'>
+            <h2>ROBUX PRÁTICAMENTE DE GRAÇA!</h2>
+            <h1>COMPRE JÁ!</h1>
+             <button className='btn'>Comprar</button>
+         </div>
+
+            </section>
+    )
+}
